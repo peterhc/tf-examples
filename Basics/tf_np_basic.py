9a11e8f49ee855fae7f868933bb722aca451efc8
Peter H Cheng <phcheng888@gmail.com>
2018-01-12 11:51:22 -0800

    tf.matmul() vs np.dot()

--- Basics/tf_np_basic.py
+++ Basics/tf_np_basic.py
@@ -1,46 +1,56 @@
 import tensorflow as tf
 import numpy as np
 
-# Matrix Multiplication using Tensorflow
+
+# 2-D tensor `a`
+# [[1, 2, 3],
+#  [4, 5, 6]]
+X = np.matrix([[1, 2, 3], [4, 5, 6]])
+print('X=\n', X)
+# 2-D tensor `b`
+# [[ 7,  8],
+#  [ 9, 10],
+#  [11, 12]]
+Y = np.matrix([[7, 8], [9, 10], [11, 12]])
+print('Y=\n', Y)
+
+# Matrix Multiplication in Tensorflow
 # 2-D a*b
 # 2-D tensor `a`
 # [[1, 2, 3],
 #  [4, 5, 6]]
-a = tf.constant([1, 2, 3, 4, 5, 6], shape=[2, 3])
+# a = tf.constant([1, 2, 3, 4, 5, 6], shape=[2, 3])
+a = tf.constant(X, dtype=tf.int32, shape=[2, 3])
 
 # 2-D tensor `b`
 # [[ 7,  8],
 #  [ 9, 10],
 #  [11, 12]]
-b = tf.constant([7, 8, 9, 10, 11, 12], shape=[3, 2])
+# b = tf.constant([7, 8, 9, 10, 11, 12], shape=[3, 2])
+b = tf.constant(Y, dtype=tf.int32, shape=[3, 2])
 
-# `a` * `b`
-# [[ 58,  64],
-#  [139, 154]]
-c = tf.matmul(a, b)
 # `a` * `b`
 # [[ 58,  64],
 #  [139, 154]]
 with tf.Session() as sess:
-    print('==> Matrix Multiplication using Tensorflow. Result=\n', sess.run(c))
+    print('==> Matrix Multiplication using tf.matlul(). Result=\n', sess.run(tf.matmul(a, b)))
 
 #==============================
 
-# Matrix Multiplication using Numpy
-X1 = np.matrix([[1, 2, 3], [4, 5, 6]])
-print('X1=', X1)
-W1 = np.matrix([[7, 8], [9, 10], [11, 12]])
-print('W1', W1)
-print('==> Matrix Multiplication using Numpy. Result=\n', np.dot(X1, W1))
+# Matrix dot Product using np.dot()
+print('==> Matrix dot Product using np.dot(). Result=\n', np.dot(X, Y))
 
 #==============================
 
-print('==> Element-wise Product using Numpy')
+# Element-wise Multiplication using np.multiply()
 X1 = np.array([[1, 2], [3, 4]])
-print('X1=', X1)
+print('X1=\n', X1)
 W1 = np.array([[5, 6], [7, 8]])
-print('W1', W1)
-print('==> Element-wise Product using Numpy. Result =\n', np.multiply(X1, W1))
+print('W1=\n', W1)
+print('==> Element-wise Product using np.multiply(). Result =\n', np.multiply(X1, W1))
+
+# Error: ValueError: operands could not be broadcast together with shapes (2,3) (3,2)
+# print('==> Element-wise Product using Numpy. Result =\n', np.multiply(X, Y))
 
 #==============================
 
@@ -68,7 +78,7 @@ b = tf.constant(np.arange(13, 25, dtype=np.int32), shape=[2, 3, 2])
 #  [[508, 532],
 #   [697, 730]]]
 with tf.Session() as sess:
-    print('==> 3D Tensor Matrix Multiplication Result=\n', sess.run(tf.matmul(a, b)))
+    print('==> 3D Tensor Matrix using tf.matmul() Result=\n', sess.run(tf.matmul(a, b)))
 
 #==============================
 
@@ -84,16 +94,3 @@ with tf.Session() as sess:
 
 #==============================
 
-# X1:
-# [[0, 0, 0],
-#  [0, 0, 0],
-#  [0, 2, 0]]
-X1 = np.matrix([[0, 0, 0], [0, 0, 0], [0, 2, 0]])
-print('X1=\n', X1)
-# W1:
-# [[1, 0, -1],
-#  [-1, -1, 0],
-#  [1, 1, 0]]
-W1 = np.matrix([[1, 0, -1], [-1, -1, 0], [1, 1, 0]])
-print('W1=\n', W1)
-print('Result=\n', np.dot(X1, W1))
