d69de65e7d19b56e8651a3bedf22a431bb3968b2
Peter <phcheng888@gmail.com>
2021-02-15 09:21:44 -0800

    Removed

--- Basics/tf_hello_world.py
+++
@@ -1,8 +0,0 @@
-# Python
-import tensorflow as tf
-print ("Tensorflow version:",tf.__version__) # Tensorflow version info
-
-hello = tf.constant('Hello, TensorFlow!')
-
-with tf.Session() as sess:
-    print(sess.run(hello))
